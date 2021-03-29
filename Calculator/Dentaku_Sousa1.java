e85526a731a401b04d87c7e5a0843a65291a683f
seastar3 <seastar@orion.nifty.jp>
2021-03-29 15:07:52 +0900

    Create Dentaku_Sousa1.java

---
+++ Calculator/Dentaku_Sousa1.java
@@ -0,0 +1,13 @@
+public class Dentaku_Sousa1 {
+  public static void main( String args[] ) {
+    Dentaku1 d1 = new Dentaku1();
+    d1.tasu(5.0);
+    d1.hiku(2.0);
+    System.out.println("答は" + d1.tasu(0.0));
+
+    Dentaku2 d1 = new Dentaku1();
+    d2.tasu(7.0);
+    d2.hiku(d1.tasu(0.0));
+    System.out.println("答は" + d2.tasu(0.0));
+  }
+}
