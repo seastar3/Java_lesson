d8147435095cb1e15af3790cb0f2eec2d596bec6
seastar3 <seastar@orion.nifty.jp>
2021-03-29 15:06:43 +0900

    Create Dentaku1.java

---
+++ Calculator/Dentaku1.java
@@ -0,0 +1,9 @@
+public class Dentaku1 {
+  Double kotae = 0.0;
+  public Double tasu(Double kazu) {
+    return kotae += kazu;
+  }
+  public Double hiku(Double kazu) {
+    return kotae -= kazu;
+  }
+}
