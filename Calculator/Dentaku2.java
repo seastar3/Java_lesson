28d26ff87aafb599eb41f9bee8026d512e768dd0
seastar3 <seastar@orion.nifty.jp>
2021-03-29 15:07:21 +0900

    Create Dentaku2.java

---
+++ Calculator/Dentaku2.java
@@ -0,0 +1,8 @@
+public class Dentaku2 extends Dentaku1 {
+  public Double kakeru(Double kazu) {
+    return kotae *= kazu;
+  }
+  public Double waru(Double kazu) {
+    return kotae *= kazu;
+  }
+}
